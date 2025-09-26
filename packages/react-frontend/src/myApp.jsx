6097e52f7ca220eb82daf23e482c0a79818fecf0
Anthony <atungnac@calpoly.edu>
2025-09-25 17:01:11 -0700

    start of lab1

---
+++ packages/react-frontend/src/myApp.jsx
@@ -0,0 +1,15 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table";
+
+function MyApp() {
+  return (
+   // <div>
+     // <h1>Hello, React!</h1>
+    //</div>
+	<div className="container">
+		<Table />
+	</div>
+  );
+}
+export default MyApp;
