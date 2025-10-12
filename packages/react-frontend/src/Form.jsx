561f00f7ac30669899b5e7cb22bea0751b2cb10f
Anthony <atungnac@calpoly.edu>
2025-10-12 16:11:20 -0700

    fix loose thing from old debug

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -4,8 +4,7 @@ import React, { useState } from "react";
 function Form(props) {
 	const [person, setPerson] = useState({
 		name: "",
-		job: "",
-		id: ""
+		job: ""
 	});
 	function handleChange(event) {
 		const { name, value } = event.target;
